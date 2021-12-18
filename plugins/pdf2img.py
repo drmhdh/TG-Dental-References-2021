bc4e992ee9bd016a79ee21589c9f175642ec1f78
Dr.Strange <71588447+drmhdh@users.noreply.github.com>
2021-12-19 03:15:06 +0530

    Update pdf2img.py

--- plugins/pdf2img.py
+++ plugins/pdf2img.py
@@ -14,7 +14,40 @@ logger = logging.getLogger(__name__)
 
 if not os.path.isdir("pdf"):
     os.mkdir("pdf")
- 
+    
+    
+async def eor(event, text, **args):
+    link_preview = args.get("link_preview", False)
+    parse_mode = args.get("parse_mode", "md")
+    time = args.get("time", None)
+    if not event.out:
+        reply_to = event.reply_to_msg_id or event
+        ok = await event.client.send_message(
+            event.chat_id,
+            text,
+            link_preview=link_preview,
+            parse_mode=parse_mode,
+            reply_to=reply_to,
+        )
+    else:
+        ok = await event.edit(text, link_preview=link_preview, parse_mode=parse_mode)
+    if not DEL_TIME:
+        from .. import udB
+
+        ut = udB.get("DEL_DELAY_TIME")
+        DEL_TIME.append(ut)
+    else:
+        ut = DEL_TIME[0]
+    if time and ut != "None":
+        time = ut or time
+        await asyncio.sleep(int(time))
+        return await ok.delete()
+    return ok
+
+DEL_TIME = []
+async def eod(event, text, **kwargs):
+    kwargs["time"] = kwargs.get("time", 5)
+    return await eor(event, text, **kwargs)
 
 def should_allow_sudo():
     from .. import udB
