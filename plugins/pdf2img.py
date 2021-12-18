dfb8ff2b23ad0ae129dd5dba0f430d5d22a710c4
Dr.Strange <71588447+drmhdh@users.noreply.github.com>
2021-12-18 19:54:22 +0530

    Update pdf2img.py

--- plugins/pdf2img.py
+++ plugins/pdf2img.py
@@ -78,7 +78,22 @@ if not os.path.isdir("pdf"):
     else:
         ok = await event.edit(text, link_preview=link_preview, parse_mode=parse_mode)"""
 # get the pattern from the decorator
-if pattern is not None:
+"""if pattern is not None:
+    if pattern.startswith("\#"):
+            # special fix for snip.py
+        args["pattern"] = re.compile(pattern)
+    else:
+        args["pattern"] = re.compile("\." + pattern)
+        cmd = "." + pattern
+        try:
+            CMD_LIST[file_test].append(cmd)
+        except:
+            CMD_LIST.update({file_test: [cmd]})"""
+    
+@Client.on_message(event.NewMessage(
+    
+    pattern="pdf ?(.*)"
+    if pattern is not None:
     if pattern.startswith("\#"):
             # special fix for snip.py
         args["pattern"] = re.compile(pattern)
@@ -90,8 +105,6 @@ if pattern is not None:
         except:
             CMD_LIST.update({file_test: [cmd]})
     
-@Client.on_message(event.NewMessage(
-    pattern="pdf ?(.*)"
 ))
 #@Client.on_message(filter.command(["pdf"]))
     
