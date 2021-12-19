edf6c18e8d0afe664805248d086e994781e4eca9
Dr.Strange <71588447+drmhdh@users.noreply.github.com>
2021-12-19 15:10:48 +0530

    Update pdf2img.py

--- plugins/pdf2img.py
+++ plugins/pdf2img.py
@@ -915,7 +915,7 @@ async def answer(client, callbackQuery):
                         pix = page.getPixmap(matrix = mat)
                         cnvrtpg += 1
                         
-                        await clientt.edit_message_text(
+                        await client.edit_message_text(
                             chat_id = callbackQuery.message.chat.id,
                             message_id = callbackQuery.message.message_id,
                             text = f"`Converted: {cnvrtpg}/{int((PAGENOINFO[callbackQuery.message.chat.id][2])+1 - int(PAGENOINFO[callbackQuery.message.chat.id][1]))} pages.. 🤞`"
