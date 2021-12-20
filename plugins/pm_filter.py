9b83648a8c03a9dd9ae2c373336eefb406a3e052
Dr.Strange <71588447+drmhdh@users.noreply.github.com>
2021-12-20 19:03:30 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -136,8 +136,7 @@ async def filter(client, message):
                 )
                 return
         except UserNotParticipant:
-            if checksub#{file_id} = None:
-            pass
+            
             await client.send_message(
                 chat_id=message.from_user.id,
                 text="**🔊 𝗝𝗼𝗶𝗻 𝗢𝘂𝗿 𝗠𝗮𝗶𝗻 𝗖𝗵𝗮𝗻𝗻𝗲𝗹 🤭** \n \n Are You Looking for References ?! \n Then First Join Our 🦷𝔻𝕖𝕟𝕥𝕒𝕝 ℂ𝕒𝕤𝕖 𝕊𝕥𝕦𝕕𝕪🔎 Channel...😁 Then Try Again... Press /start 😁 and You will Get Your Requests Here...! \n \n 🪐Powered by: \n 🔬 @dent_tech_for_u 📚",
