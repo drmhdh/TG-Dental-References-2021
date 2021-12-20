c06bbef9bfa98459071fc33415657311a26e5866
Dr.Strange <71588447+drmhdh@users.noreply.github.com>
2021-12-20 18:25:07 +0530

    Update pdf2image.py

--- plugins/pdf2image.py
+++ plugins/pdf2image.py
@@ -1003,7 +1003,7 @@ async def answer(client, callbackQuery):
                             await client.send_media_group(
                                 callbackQuery.message.chat.id,
                                 media[callbackQuery.message.chat.id],
-                                reply_to_message_id = message.reply_to_message.message_id
+                               
                             )
                             
                         except Exception:
