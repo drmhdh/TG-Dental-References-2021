fbe2970bfc1d19960e122faf4cfa764430f3ed6b
Dr.Strange <71588447+drmhdh@users.noreply.github.com>
2022-01-07 03:07:21 +0530

    Update info.py

--- info.py
+++ info.py
@@ -30,6 +30,8 @@ AUTH_USERS = (auth_users + ADMINS) if auth_users else []
 auth_channel = environ.get('AUTH_CHANNEL')
 AUTH_CHANNEL = int(auth_channel) if auth_channel and id_pattern.search(auth_channel) else auth_channel
 AUTH_GROUPS = [int(admin) for admin in environ.get("AUTH_GROUPS", "").split()]
+INDEX_REQ_CHANNEL = int(environ.get('INDEX_REQ_CHANNEL', LOG_CHANNEL))
+FILE_STORE_CHANNEL = [int(ch) for ch in (environ.get('FILE_STORE_CHANNEL', '')).split()]
 
 
 # Others
