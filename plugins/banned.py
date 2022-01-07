b6d135cd4c0e9ddc0829ba03710f8aff605e2e53
Dr.Strange <71588447+drmhdh@users.noreply.github.com>
2022-01-07 09:09:17 +0530

    Update banned.py

--- plugins/banned.py
+++ plugins/banned.py
@@ -1,21 +1,18 @@
-from pyrogram import Client, filters
 from utils import temp
+from info import SUPPORT_CHAT
 from pyrogram.types import Message
+from pyrogram import Client, filters
 from database.users_chats_db import db
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
-from info import SUPPORT_CHAT
+
 async def banned_users(_, client, message: Message):
     return (
         message.from_user is not None or not message.sender_chat
     ) and message.from_user.id in temp.BANNED_USERS
-
 banned_user = filters.create(banned_users)
-
 async def disabled_chat(_, client, message: Message):
     return message.chat.id in temp.BANNED_CHATS
-
 disabled_group=filters.create(disabled_chat)
-
 
 @Client.on_message(filters.private & banned_user & filters.incoming)
 async def ban_reply(bot, message):
