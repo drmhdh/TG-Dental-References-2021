db3a2db3de53133653c68408c6b55e1c995c209b
Dr.Strange <71588447+drmhdh@users.noreply.github.com>
2022-01-07 09:03:32 +0530

    Update filters.py

--- plugins/filters.py
+++ plugins/filters.py
@@ -1,20 +1,18 @@
 import io
+import pyrogram
+from info import ADMINS
 from pyrogram import filters, Client
+from database.users_chats_db import db
+from utils import get_file_id, parser, split_quotes
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+from database.ia_filterdb import Media, get_file_details, get_search_results
+from database.connections_mdb import active_connection, all_connections, delete_connection, if_active, make_active, make_inactive
 from database.filters_mdb import(
    add_filter,
    get_filters,
    delete_filter,
    count_filters
 )
-import pyrogram
-from database.connections_mdb import active_connection, all_connections, delete_connection, if_active, make_active, make_inactive
-from database.users_chats_db import db
-from database.ia_filterdb import Media, get_file_details, get_search_results
-
-from utils import get_file_id, parser, split_quotes
-from info import ADMINS
-
 
 @Client.on_message(filters.command(['filter', 'add']) & filters.incoming)
 async def addfilter(client, message):
@@ -23,7 +21,6 @@ async def addfilter(client, message):
         return await message.reply(f"You are anonymous admin. Use /connect {message.chat.id} in PM")
     chat_type = message.chat.type
     args = message.text.html.split(None, 1)
-
     if chat_type == "private":
         grpid = await active_connection(str(userid))
         if grpid is not None:
@@ -37,14 +34,11 @@ async def addfilter(client, message):
         else:
             await message.reply_text("I'm not connected to any groups!", quote=True)
             return
-
     elif chat_type in ["group", "supergroup"]:
         grp_id = message.chat.id
         title = message.chat.title
-
     else:
         return
-
     st = await client.get_chat_member(grp_id, userid)
     if (
         st.status != "administrator"
@@ -52,26 +46,21 @@ async def addfilter(client, message):
         and str(userid) not in ADMINS
     ):
         return
-
-
     if len(args) < 2:
         await message.reply_text("Command Incomplete :(", quote=True)
         return
 
     extracted = split_quotes(args[1])
     text = extracted[0].lower()
-
     if not message.reply_to_message and len(extracted) < 2:
         await message.reply_text("Add some content to save your filter!", quote=True)
         return
-
     if (len(extracted) >= 2) and not message.reply_to_message:
         reply_text, btn, alert = parser(extracted[1], text)
         fileid = None
         if not reply_text:
             await message.reply_text("You cannot have buttons alone, give some text to go with it!", quote=True)
             return
-
     elif message.reply_to_message and message.reply_to_message.reply_markup:
         try:
             rm = message.reply_to_message.reply_markup
@@ -89,7 +78,6 @@ async def addfilter(client, message):
             btn = "[]" 
             fileid = None
             alert = None
-
     elif message.reply_to_message and message.reply_to_message.media:
         try:
             msg = get_file_id(message.reply_to_message)
@@ -118,10 +106,8 @@ async def addfilter(client, message):
         parse_mode="md"
     )
 
-
 @Client.on_message(filters.command(['viewfilters', 'filters']) & filters.incoming)
-async def get_all(client, message):
-    
+async def get_all(client, message):    
     chat_type = message.chat.type
     userid = message.from_user.id if message.from_user else None
     if not userid:
@@ -140,14 +126,11 @@ async def get_all(client, message):
         else:
             await message.reply_text("I'm not connected to any groups!", quote=True)
             return
-
     elif chat_type in ["group", "supergroup"]:
         grp_id = message.chat.id
         title = message.chat.title
-
     else:
         return
-
     st = await client.get_chat_member(grp_id, userid)
     if (
         st.status != "administrator"
@@ -155,17 +138,13 @@ async def get_all(client, message):
         and str(userid) not in ADMINS
     ):
         return
-
     texts = await get_filters(grp_id)
     count = await count_filters(grp_id)
     if count:
         filterlist = f"Total number of filters in **{title}** : {count}\n\n"
-
         for text in texts:
             keywords = " ×  `{}`\n".format(text)
-
             filterlist += keywords
-
         if len(filterlist) > 4096:
             with io.BytesIO(str.encode(filterlist.replace("`", ""))) as keyword_file:
                 keyword_file.name = "keywords.txt"
@@ -176,7 +155,6 @@ async def get_all(client, message):
             return
     else:
         filterlist = f"There are no active filters in **{title}**"
-
     await message.reply_text(
         text=filterlist,
         quote=True,
@@ -189,7 +167,6 @@ async def deletefilter(client, message):
     if not userid:
         return await message.reply(f"You are anonymous admin. Use /connect {message.chat.id} in PM")
     chat_type = message.chat.type
-
     if chat_type == "private":
         grpid  = await active_connection(str(userid))
         if grpid is not None:
@@ -206,10 +183,8 @@ async def deletefilter(client, message):
     elif chat_type in ["group", "supergroup"]:
         grp_id = message.chat.id
         title = message.chat.title
-
     else:
         return
-
     st = await client.get_chat_member(grp_id, userid)
     if (
         st.status != "administrator"
@@ -217,7 +192,6 @@ async def deletefilter(client, message):
         and str(userid) not in ADMINS
     ):
         return
-
     try:
         cmd, text = message.text.split(" ", 1)
     except:
@@ -228,19 +202,15 @@ async def deletefilter(client, message):
             quote=True
         )
         return
-
     query = text.lower()
-
     await delete_filter(message, query, grp_id)
         
-
 @Client.on_message(filters.command('delall') & filters.incoming)
 async def delallconfirm(client, message):
     userid = message.from_user.id if message.from_user else None
     if not userid:
         return await message.reply(f"You are anonymous admin. Use /connect {message.chat.id} in PM")
     chat_type = message.chat.type
-
     if chat_type == "private":
         grpid  = await active_connection(str(userid))
         if grpid is not None:
@@ -258,10 +228,8 @@ async def delallconfirm(client, message):
     elif chat_type in ["group", "supergroup"]:
         grp_id = message.chat.id
         title = message.chat.title
-
     else:
         return
-
     st = await client.get_chat_member(grp_id, userid)
     if (st.status == "creator") or (str(userid) in ADMINS):
         await message.reply_text(
