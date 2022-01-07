10f674cfbf75c81388ebbf0a06f074dcdcc5c7b8
Dr.Strange <71588447+drmhdh@users.noreply.github.com>
2022-01-07 08:58:07 +0530

    Update inline.py

--- plugins/inline.py
+++ plugins/inline.py
@@ -1,25 +1,23 @@
 import logging
 from pyrogram import Client, emoji, filters
-from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, InlineQueryResultCachedDocument
-from pyrogram.errors.exceptions.bad_request_400 import QueryIdInvalid
-from utils import get_search_results, is_subscribed, get_size
-from info import CACHE_TIME, AUTH_USERS, AUTH_CHANNEL, CUSTOM_FILE_CAPTION
 from database.ia_filterdb import get_search_results
+from utils import get_search_results, is_subscribed, get_size
+from pyrogram.errors.exceptions.bad_request_400 import QueryIdInvalid
+from info import CACHE_TIME, AUTH_USERS, AUTH_CHANNEL, CUSTOM_FILE_CAPTION
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, InlineQueryResultCachedDocument
+
 logger = logging.getLogger(__name__)
 cache_time = 0 if AUTH_USERS or AUTH_CHANNEL else CACHE_TIME
-
 
 @Client.on_inline_query(filters.user(AUTH_USERS) if AUTH_USERS else None)
 async def answer(bot, query):
     """Show search results for given inline query"""
-
     if AUTH_CHANNEL and not await is_subscribed(bot, query):
         await query.answer(results=[],
                            cache_time=0,
                            switch_pm_text='You have to subscribe my channel to use the bot',
                            switch_pm_parameter="subscribe")
         return
-
     results = []
     if '|' in query.query:
         string, file_type = query.query.split('|', maxsplit=1)
@@ -28,22 +26,19 @@ async def answer(bot, query):
     else:
         string = query.query.strip()
         file_type = None
-
     offset = int(query.offset or 0)
     reply_markup = get_reply_markup(query=string)
     files, next_offset, total = await get_search_results(string,
                                                   file_type=file_type,
                                                   max_results=10,
                                                   offset=offset)
-
-    for file in files:
+    for file in files:                                 
         title=file.file_name
         size=get_size(file.file_size)
         f_caption=file.caption
         if CUSTOM_FILE_CAPTION:
             try:
-                f_caption=CUSTOM_FILE_CAPTION.format(file_name= '' if title is None else title, file_size='' if size is None else size, file_caption='' if f_caption is None else f_caption)
-  
+                f_caption=CUSTOM_FILE_CAPTION.format(file_name= '' if title is None else title, file_size='' if size is None else size, file_caption='' if f_caption is None else f_caption)  
             except Exception as e:
                 print(e)
                 f_caption=f_caption
@@ -80,13 +75,11 @@ async def answer(bot, query):
         switch_pm_text = f'{emoji.CROSS_MARK} No results'
         if string:
             switch_pm_text += f' for "{string}"'
-
         await query.answer(results=[],
                            is_personal = True,
                            cache_time=cache_time,
                            switch_pm_text=switch_pm_text,
                            switch_pm_parameter="okay")
-
 
 def get_reply_markup(query):
     buttons = [
