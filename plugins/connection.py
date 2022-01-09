f7e1dbc6b5befc46ffa5e32ba1ceac2380cc9dde
Dr.Strange <71588447+drmhdh@users.noreply.github.com>
2022-01-09 05:52:47 +0530

    Update connection.py

--- plugins/connection.py
+++ plugins/connection.py
@@ -54,7 +54,7 @@ async def addconnection(client,message):
             addcon = await add_connection(str(group_id), str(userid))
             if addcon:
                 await message.reply_text(
-                    f"Sucessfully connected to **{title}**\nNow manage your group from my pm !",
+                    f"Successfully connected to **{title}**\nNow manage your group from my pm !",
                     quote=True,
                     parse_mode="md"
                 )
