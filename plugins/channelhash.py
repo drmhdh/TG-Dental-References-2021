555cb08c76af1088d5fe44ac1e42fda059776cf2
Dr.Strange <71588447+drmhdh@users.noreply.github.com>
2022-01-12 20:55:09 +0530

    Create channelhash.py

---
+++ plugins/channelhash.py
@@ -0,0 +1,12 @@
+
+
+if message.text.startswith("#request"):
+    try:
+        req=message.text.replace("#request", " ")
+        if req == " ":      
+            await message.reply("What..?")
+        else:
+             await client.send_message(chat_id=int("-1001754998233"), text=f"<b>#NewRequest\n \n \n🧿 Movie Name:{req}\n \n🧿 Requested By: {message.from_user.mention}\n \n🧿 User ID:</b> <code>{message.from_user.id}</code>\n \n<b>🧿 Chat: {message.chat.title}\n \n🧿 Chat ID:</b> <code>{message.chat.id}</code>")
+             await message.reply("<b>Your request successfully submitted to admins ✅\nThey will add it as soon as possible!<b>")
+    except Exception as e:
+        await message.reply(f"Error occurred!\n \n{e}")
