c96db919ca5012ae9e4d45f62c18be8919e93e55
Dr.Strange <71588447+drmhdh@users.noreply.github.com>
2022-01-13 00:47:12 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -914,7 +914,7 @@ async def hashrequests(client, message, text=False):
             await message.reply(f"Error occurred!\n \n{e}")       
 
  
-  if not medsage.text & message.caption.startswith("#request"):
+  if not message.text & message.caption.startswith("#request"):
         try:
             
             requ=message.caption.replace("#request", " ")
