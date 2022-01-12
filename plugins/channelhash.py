f9d0b75737c66069ae127d5ffed6e3d293200c33
Dr.Strange <71588447+drmhdh@users.noreply.github.com>
2022-01-12 21:06:29 +0530

    Update channelhash.py

--- plugins/channelhash.py
+++ plugins/channelhash.py
@@ -1,3 +1,10 @@
+import os
+from pyrogram import filters, Client
+
+
+@Client.on_message(filters.incoming & ~filters.edited)
+#async def start(client, message):       
+
 
 
 if message.text.startswith("#request"):
@@ -6,7 +13,7 @@ if message.text.startswith("#request"):
         if req == " ":      
             await message.reply("What..?")
         else:
-             await client.send_message(chat_id=int("-1001754998233"), text=f"<b>#NewRequest\n \n \n🧿 Movie Name:{req}\n \n🧿 Requested By: {message.from_user.mention}\n \n🧿 User ID:</b> <code>{message.from_user.id}</code>\n \n<b>🧿 Chat: {message.chat.title}\n \n🧿 Chat ID:</b> <code>{message.chat.id}</code>")
+             await client.send_message(chat_id=int("-1001110994526"), text=f"<b>#NewRequest\n \n \n🧿 Book's Name:{req}\n \n🧿 Requested By: {message.from_user.mention}\n \n🧿 User ID:</b> <code>{message.from_user.id}</code>\n \n<b>🧿 Chat: {message.chat.title}\n \n🧿 Chat ID:</b> <code>{message.chat.id}</code>")
              await message.reply("<b>Your request successfully submitted to admins ✅\nThey will add it as soon as possible!<b>")
     except Exception as e:
         await message.reply(f"Error occurred!\n \n{e}")
