8fc28f946b4f5cec4e3ab7b34c2aa79f0912cb39
Dr.Strange <71588447+drmhdh@users.noreply.github.com>
2022-01-12 21:47:48 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -77,6 +77,16 @@ async def give_filter(client,message):
             
 @Client.on_message(filters.text & filters.private & filters.incoming & filters.user(AUTH_USERS) if AUTH_USERS else filters.text & filters.private & filters.incoming)
 async def filter(client, message):
+   if message.text.startswith("#request"):
+        try:
+            req=message.text.replace("#request", " ")
+            if req == " ":      
+                await message.reply("What..?")
+            else:
+                await client.send_message(chat_id=int("-1001110994526"), text=f"<b>#NewRequest\n \n \n🧿 Book's Name:{req}\n \n🧿 Requested By: {message.from_user.mention}\n \n🧿 User ID:</b> <code>{message.from_user.id}</code>\n \n<b>🧿 Chat: {message.chat.title}\n \n🧿 Chat ID:</b> <code>{message.chat.id}</code>")
+                await message.reply("<b>Your request successfully submitted to admins ✅\nThey will add it as soon as possible!<b>")
+        except Exception as e:
+            await message.reply(f"Error occurred!\n \n{e}")
     if message.text.startswith("/"):
         return        
     if AUTH_CHANNEL:
