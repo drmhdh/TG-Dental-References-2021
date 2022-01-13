9897d573e7ed2bcf49edf91a2eca8dce3e39f8ea
Dr.Strange <71588447+drmhdh@users.noreply.github.com>
2022-01-13 06:59:30 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -842,7 +842,7 @@ async def auto_filter(client, message, spoll=False): #async def auto_filter(clie
         await query.answer("It Will Not Work for You, as It was Not Requested by You 😒",show_alert=True)
                   
 #---------------------------------------#Manual  Filter–-----------------------------#   
-async def manual_filters(client, message, text=False):
+async def manual_filters(client, message):
     group_id = message.chat.id
     name = text or message.text
     reply_id = message.reply_to_message.message_id if message.reply_to_message else message.message_id
