322f237dd630105381cacd49fa13092c22ede2cf
Dr.Strange <71588447+drmhdh@users.noreply.github.com>
2022-01-13 15:47:16 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -891,7 +891,7 @@ async def manual_filters(client, message, text=False):
         return False
   #----------------------------------------#     
 
-async def hashrequests(client, message, text=False or True):
+async def hashrequests(client, message):
     title=message.caption
     if title.startswith("#request"):
         try:
