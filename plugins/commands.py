fbd08f943c3558886f221cc1c8a4d808d31c35e1
Dr.Strange <71588447+drmhdh@users.noreply.github.com>
2022-01-20 06:32:48 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -537,107 +537,110 @@ async def delete_all_index_confirm(bot, message):
   
 @Client.on_message(filters.command('settings'))
 async def settings(client, message):
-    userid = message.from_user.id if message.from_user else None
-    if not userid:
-        return await message.reply(f"You are anonymous admin. Use /connect {message.chat.id} in PM")
-    chat_type = message.chat.type
+    try:
+        userid = message.from_user.id if message.from_user else None
+        if not userid:
+            await message.reply(f"You are anonymous admin. Use /connect {message.chat.id} in PM")
+        chat_type = message.chat.type
     
 
-    if chat_type == "private":
-        grpid = await active_connection(str(userid))
-        if grpid is not None:
-            grp_id = grpid
-            try:
-                chat = await client.get_chat(grpid)
-                title = chat.title
-            except:
-                await message.reply_text("Make sure I'm present in your group!!", quote=True)
+        if chat_type == "private":
+            grpid = await active_connection(str(userid))
+            if grpid is not None:
+                grp_id = grpid
+                try:
+                    chat = await client.get_chat(grpid)
+                    title = chat.title
+                except:
+                    await message.reply_text("Make sure I'm present in your group!!", quote=True)
+                    return
+            else:
+                await message.reply_text("I'm not connected to any groups!", quote=True)
                 return
+
+        elif chat_type in ["group", "supergroup"]:
+            grp_id = message.chat.id
+            title = message.chat.title
+
         else:
-            await message.reply_text("I'm not connected to any groups!", quote=True)
             return
 
-    elif chat_type in ["group", "supergroup"]:
-        grp_id = message.chat.id
-        title = message.chat.title
-
-    else:
-        return
-
-    st = await client.get_chat_member(grp_id, userid)
-    if (st.status != "administrator" and st.status != "creator" and str(userid) not in ADMINS):
-        return
+        st = await client.get_chat_member(grp_id, userid)
+        if (st.status != "administrator" and st.status != "creator" and str(userid) not in ADMINS):
+            return
             
             
     
         
 
 
-    settings = await get_settings(grp_id)
+        settings = await get_settings(grp_id)
 
-    if settings is not None:
-        await message.reply_text(
+        if settings is not None:
+            await message.reply_text(
             
-            text=f"<b>Change Your Settings for {title} As Your Wish ⚙</b>",
+                text=f"<b>Change Your Settings for {title} As Your Wish ⚙</b>",
             
             
             
-            reply_to_message_id=message.message_id,
-            disable_web_page_preview=True,
-            parse_mode="html",
-                reply_markup = InlineKeyboardMarkup(
+                reply_to_message_id=message.message_id,
+                disable_web_page_preview=True,
+                parse_mode="html",
+                    reply_markup = InlineKeyboardMarkup(
             
-                    [
+                        [
                         
                     
                     
-                        InlineKeyboardButton(
-                            'Bot PM',
-                            callback_data=f'setgs#botpm#{settings["botpm"]}#{grp_id}',
-                        ),
-                        InlineKeyboardButton(
-                            '✅ Yes' if settings["botpm"] else '❌ No',
-                            callback_data=f'setgs#botpm#{settings["botpm"]}#{grp_id}',
-                        ),
-                    ],
-                    [
-                        InlineKeyboardButton(
-                            'File Secure',
+                            InlineKeyboardButton(
+                                'Bot PM',
+                                callback_data=f'setgs#botpm#{settings["botpm"]}#{grp_id}',
+                            ),
+                            InlineKeyboardButton(
+                                '✅ Yes' if settings["botpm"] else '❌ No',
+                                callback_data=f'setgs#botpm#{settings["botpm"]}#{grp_id}',
+                            ),
+                        ],
+                        [
+                            InlineKeyboardButton(
+                                'File Secure',
                             callback_data=f'setgs#file_secure#{settings["file_secure"]}#{grp_id}',
                         ),
-                        InlineKeyboardButton(
-                            '✅ Yes' if settings["file_secure"] else '❌ No',
-                            callback_data=f'setgs#file_secure#{settings["file_secure"]}#{grp_id}',
-                        ),
-                    ],
-                    [
-                        InlineKeyboardButton(
-                            'IMDB',
-                            callback_data=f'setgs#imdb#{settings["imdb"]}#{grp_id}',
-                        ),
-                        InlineKeyboardButton(
-                            '✅ Yes' if settings["imdb"] else '❌ No',
-                            callback_data=f'setgs#imdb#{settings["imdb"]}#{grp_id}',
-                        ),
-                    ],
+                            InlineKeyboardButton(
+                                '✅ Yes' if settings["file_secure"] else '❌ No',
+                                callback_data=f'setgs#file_secure#{settings["file_secure"]}#{grp_id}',
+                            ),
+                        ],
+                        [
+                            InlineKeyboardButton(
+                                'IMDB',
+                                callback_data=f'setgs#imdb#{settings["imdb"]}#{grp_id}',
+                            ),
+                            InlineKeyboardButton(
+                                '✅ Yes' if settings["imdb"] else '❌ No',
+                                callback_data=f'setgs#imdb#{settings["imdb"]}#{grp_id}',
+                            ),
+                        ],
                     
                         
                     
-                    [
-                         InlineKeyboardButton(
-                            'Welcome',
-                            callback_data=f'setgs#welcome#{settings["welcome"]}#{grp_id}',
-                        ),
-                        InlineKeyboardButton(
-                            '✅ Yes' if settings["welcome"] else '❌ No',
-                            callback_data=f'setgs#welcome#{settings["welcome"]}#{grp_id}',
-                        ),
-                    ],
+                        [
+                             InlineKeyboardButton(
+                                 'Welcome',
+                                 callback_data=f'setgs#welcome#{settings["welcome"]}#{grp_id}',
+                            ),
+                            InlineKeyboardButton(
+                                '✅ Yes' if settings["welcome"] else '❌ No',
+                                callback_data=f'setgs#welcome#{settings["welcome"]}#{grp_id}',
+                            ),
+                        ],
+                    )
                 )
-            )
             
             
-                
+    except Exception as e:
+        print(e)
+        
             
         
         
